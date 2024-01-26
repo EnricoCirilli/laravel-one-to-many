1cda82347a698590b5fa040c7c0920d3e63d0355
EnricoCirilli <enrico.cirilli@gmail.com>
2024-01-26 19:56:29 +0100

    add: validate

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,23 +1,24 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8 mt-4">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-            </div>
+    <div class="container mt-5">
+        <h2>{{ $project->title }}</h2>
+        <div>
+            <p>
+           tipologia: {{ $project->type ? $project->type->name : 'nessuna tipologia' }}
+            </p>
         </div>
+
+        <div class="mt-4">
+            Data: {{ $project->created_at }}
+        </div>
+
+        <div class="mt-4">
+            Slug: {{ $project->slug }}
+        </div>
+
+        <p class="mt-4">
+            {{ $project->description }}
+        </p>
     </div>
 @endsection
