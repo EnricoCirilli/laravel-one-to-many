2e3b23b37d2c846636f2908a4372596f6e0ce25d
EnricoCirilli <enrico.cirilli@gmail.com>
2024-01-26 11:41:26 +0100

    add

--- database/migrations/2024_01_26_102649_create_nomi_table.php
+++ database/migrations/2024_01_26_102649_create_nomi_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('nomi', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nome');
         });
     }
 
