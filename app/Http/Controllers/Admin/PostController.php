79e404a3e07007b1c12e44f6eb1518ffa2dfcfac
EnricoCirilli <enrico.cirilli@gmail.com>
2024-01-26 16:54:34 +0100

    add: CRUD  function(edit,update,delete)

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\StorePostRequest;
+use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Str;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return view('admin.posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePostRequest $request)
+    {
+        $form_data = $request->validated();
+        $post = new Post();
+        $post->titolo = $form_data['titolo'];
+        $post->contenuto = $form_data['contenuto'];
+        $post->slug = Str::slug($form_data['titolo']);
+        $post->save();
+
+        return redirect()->route('admin.posts.show', ['post' => $post->id]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $post = Post::findOrFail($id);
+        return view('admin.posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+        return view('admin.posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StorePostRequest $request, $id)
+    {
+        $form_data = $request->validated();
+        $post_to_update = Post::findOrFail($id);
+        $post_to_update->titolo = $form_data['titolo'];
+        $post_to_update->contenuto = $form_data['contenuto'];
+        $post_to_update->slug = Str::slug($form_data['titolo']);
+        $post_to_update->save();
+
+        return redirect()->route('admin.posts.show', ['post' => $post_to_update->id]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+        return redirect()->route('admin.posts.index');
+    }
+}
