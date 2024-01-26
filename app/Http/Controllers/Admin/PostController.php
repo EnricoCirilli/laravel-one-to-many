3b7f9d73b48ce1117ce1930bc9fa2bad002fe855
EnricoCirilli <enrico.cirilli@gmail.com>
2024-01-26 19:50:16 +0100

    add: edit operation

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\StorePostRequest;
+use App\Http\Requests\StoreProjectRequest;
+use App\Http\Requests\UpdateProjectRequest;
 use Illuminate\Http\Request;
-use App\Models\Post;
+use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Support\Str;
 
-class PostController extends Controller
+class ProjectController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -17,8 +19,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
-        return view('admin.posts.index', compact('posts'));
+        $projects = Project::all();
+        return view('admin.projects.index', compact('projects'));
     }
 
     /**
@@ -28,7 +30,8 @@ class PostController extends Controller
      */
     public function create()
     {
-        return view('admin.posts.create');
+        $types = Type::all();
+        return view('admin.projects.create', compact('types'));
     }
 
     /**
@@ -37,71 +40,66 @@ class PostController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StorePostRequest $request)
+    public function store(StoreProjectRequest $request)
     {
         $form_data = $request->validated();
-        $post = new Post();
-        $post->titolo = $form_data['titolo'];
-        $post->contenuto = $form_data['contenuto'];
-        $post->slug = Str::slug($form_data['titolo']);
-        $post->save();
-
-        return redirect()->route('admin.posts.show', ['post' => $post->id]);
+        $project = new Project ();
+        $project->fill($form_data);
+        $project->type_id = $request->type_id;
+        // dd($request->all(), $form_data, $project);
+        // $project->slug = Str::slug ($project->title, '-');
+        $project->save();
+        return redirect()->route('admin.projects.show', ['project' => $project->slug]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param   Project $project
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Project $project)
     {
-        $post = Post::findOrFail($id);
-        return view('admin.posts.show', compact('post'));
+        return view('admin.projects.show', compact('project'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  Project $project
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Project $project)
     {
-        $post = Post::findOrFail($id);
-        return view('admin.posts.edit', compact('post'));
+        $types = Type::all();
+        return view('admin.projects.edit', compact('project','types'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  Project project
      * @return \Illuminate\Http\Response
      */
-    public function update(StorePostRequest $request, $id)
+    public function update(UpdateProjectRequest $request,Project $project)
     {
         $form_data = $request->validated();
-        $post_to_update = Post::findOrFail($id);
-        $post_to_update->titolo = $form_data['titolo'];
-        $post_to_update->contenuto = $form_data['contenuto'];
-        $post_to_update->slug = Str::slug($form_data['titolo']);
-        $post_to_update->save();
+        $project->update($form_data);
 
-        return redirect()->route('admin.posts.show', ['post' => $post_to_update->id]);
+        return redirect()->route('admin.projects.show',['project'=>$project->slug]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Project $project
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Project $project)
     {
-        $post = Post::findOrFail($id);
-        $post->delete();
-        return redirect()->route('admin.posts.index');
+        $project->delete();
+
+        return redirect()->route('admin.projects.index')->with('message',"$project->title è stato cancellato con successo!");
     }
 }
